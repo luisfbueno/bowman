d67845e3476da5da33976c7b4eb5c0874f74cc97
Renata Falguera Gonçalves <renata.falguera@gmail.com>
2016-07-27 15:34:05 -0300

    Parede Móvel Adicionada

---
+++ Assets/Scripts/Parede2.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parede2 : MonoBehaviour {
+    float speed = 2;
+    float x, z,X;
+    public float y;
+    public GameObject parede;
+    //   int ativou = 0;
+    // int colidiu = 0;
+    bool colidiu = false;
+	// Use this for initialization
+	void Start()
+    {
+        y = transform.position.y;
+        x = transform.position.x;
+        X = x;
+    }
+	// Update is called once per frame
+	void Update () {
+       
+        Vector2 input = new Vector2(transform.position.x,y);
+        Vector2 direction = input.normalized;
+        Vector2 velocity = direction * speed;
+        Vector2 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+
+        if(colidiu == false)
+        {
+            y++;
+           x = X;
+        }
+        else
+        {
+            y--;
+           x = X;
+        }
+    }
+
+     void OnTriggerEnter2D(Collider2D parede)
+      {
+          if(parede.tag == "Céu")
+          {
+              print("ok");
+              y--;
+              colidiu = true;
+          }
+
+          if(parede.tag == "Ground")
+          {
+              print("Colidiu com o chão");
+              y++;
+              colidiu = false;
+          }
+      }
+         
+
+}
---
+++ Assets/Scripts/SoltaBombaScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoltaBombaScript : MonoBehaviour {
+    float speed = 2;
+    public float x;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            Vector2 input = new Vector2(x, transform.position.y);
+            Vector2 direction = input.normalized;
+            Vector2 velocity = direction * speed;
+            Vector2 moveAmount = velocity * Time.deltaTime;
+
+            transform.Translate(moveAmount);
+            x++;
+        }
+	}
+}
