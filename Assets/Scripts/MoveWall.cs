0cb87386e8bee4deddf7c62c78106c3007639d80
Renata Falguera Gonçalves <renata.falguera@gmail.com>
2016-07-28 18:17:05 -0300

    Parede Móvel atualizada

--- Assets/Scripts/MoveWall.cs
+++ Assets/Scripts/MoveWall.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Parede2 : MonoBehaviour {
+public class MoveWall : MonoBehaviour {
+
     private float x, y;
     public Vector2 startPosition;
     public GameObject wall;
@@ -9,39 +10,37 @@ public class Parede2 : MonoBehaviour {
     bool colisao = false;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start () {
         x = transform.position.x;
         y = transform.position.y;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
         // Update the wall position
         wall.transform.position = new Vector2(x, y);
-
-        if (y >= end.y)
+         
+         if(y >= end.y)
         {
             print("1");
             y -= 0.01F;
             colisao = true;
         }
 
-        if (colisao == false)
+         if(colisao == false)
         {
             y += 0.01F;
         }
 
-        if (colisao == true)
+        if(colisao == true)
         {
             y -= 0.01F;
         }
-        if (y == startPosition.y)
+        if(y == startPosition.y)
         {
             print("2");
-            y += 0.01F;
+            y+=0.01F;
             colisao = false;
         }
     }
