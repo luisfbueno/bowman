fb3562dfa11892a4188010bd23c7bd44bf370170
Luis Felipe Bueno <luis.bueno96@gmail.com>
2016-07-30 16:10:40 -0300

    Script de correção de colisão adicionado

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -20,32 +20,9 @@ public class Arrow : MonoBehaviour
     
     public void Shoot(Vector2 differenceBetweenPoints, float magnitudeBetweenPoints, Vector2 arrowStart)
     {
-        Rigidbody2D rb = gameObject.GetComponentInChildren<Rigidbody2D>();
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
         rb.AddForce(differenceBetweenPoints.normalized * magnitudeBetweenPoints * standardForceMultiplier); //normal do vetor de distancia entre pontos* distancia entre pontos
         FindObjectOfType<GameManager>().GetComponent<GameManager>().AddTurn();
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-       
-        if(collision.gameObject.tag == "Head")
-        {
-            collision.gameObject.transform.parent.GetComponent<PlayerController>().addDamage(3);
-            print("Head");
-        }
-
-        if (collision.gameObject.tag == "Body")
-        {
-            collision.gameObject.transform.parent.GetComponent<PlayerController>().addDamage(2);
-            print("Body");
-        }
-
-        if (collision.gameObject.tag == "Legs")
-        {
-            collision.gameObject.transform.parent.GetComponent<PlayerController>().addDamage(1);
-            print("Legs");
-        }
-
     }
 
 }
---
+++ Assets/Scripts/ArrowCollisionDetector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowCollisionDetector : MonoBehaviour
+{
+
+    public float standardForceMultiplier;
+    public GameObject shootingPlayer;
+    float visibleScreen;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        if(collision.gameObject.tag == "Head")
+        {
+            collision.gameObject.transform.parent.GetComponent<PlayerController>().addDamage(3);
+            print("Head");
+        }
+
+        if (collision.gameObject.tag == "Body")
+        {
+            collision.gameObject.transform.parent.GetComponent<PlayerController>().addDamage(2);
+            print("Body");
+        }
+
+        if (collision.gameObject.tag == "Legs")
+        {
+            collision.gameObject.transform.parent.GetComponent<PlayerController>().addDamage(1);
+            print("Legs");
+        }
+
+    }
+
+}
